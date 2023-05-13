bd447b68000d07d3b6090f241b265139d76507a7
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-05-13 10:25:54 -0300

    feat: arquitecture: files and controllers created

---
+++ src/controllers/customers.controller.js
@@ -0,0 +1,7 @@
+export async function getCustomers(req, res) {
+    res.send("getCustomers");
+}
+
+export async function getCustomersById(req, res){
+    res.send("getCustomersById");
+}
---
+++ src/controllers/games.controller.js
@@ -0,0 +1,6 @@
+export async function getGames(req, res) {
+    res.send("getGames");
+}
+export async function insertGame(req, res) {
+    res.send("insertGame");
+}
