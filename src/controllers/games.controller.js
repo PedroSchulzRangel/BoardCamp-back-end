31bd5ac6528083d0e3436683ca9594355270d248
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-05-14 20:57:10 -0300

    fix: query SELECT of insertGame

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -14,13 +14,13 @@ export async function insertGame(req, res) {
     const {name, image, stockTotal, pricePerDay} = req.body;
 
     try{
-        const gameAlreadyExists = await db.query(`SELECT * FROM games WHERE name=${name};`);
+        const gameAlreadyExists = await db.query(`SELECT * FROM games WHERE name=$1;`,[name]);
         
         if(gameAlreadyExists.rowCount !== 0) return res.sendStatus(409);
         
         await db.query(`INSERT INTO games
             (name,image,"stockTotal","pricePerDay")
-            VALUES ('${name}','${image}',${stockTotal},${pricePerDay});`);
+            VALUES (${name},${image},${stockTotal},${pricePerDay});`);
         
         res.sendStatus(201);
     } catch(error){
