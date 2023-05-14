ab6acee633d3f7e081efe74b9a0499013b1bb56e
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-05-14 17:46:30 -0300

    feat: list games

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,8 +1,14 @@
 import {db} from "../database/database.connection.js"
 
 export async function getGames(req, res) {
-    
-    res.send("getGames");
+    try{
+        const resultGames = await db.query(`SELECT * FROM games;`);
+
+        res.send(resultGames.rows);
+
+    } catch(error){
+        res.status(500).send(error.message);
+    }
 }
 export async function insertGame(req, res) {
     res.send("insertGame");
